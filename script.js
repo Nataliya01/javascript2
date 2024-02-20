0ed21ae051e8f3c7a068e292373ff6b35dc093f4
Nataliya Home <vmsoft.ua@gmail.com>
2024-02-20 19:05:34 +0200

    false && true || true

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"// The expression (+null == false) < 1 is evaluated as follows:" 
-"// First, +null is compared to false. The unary plus operator (+) converts null to a number, which equals 0. Therefore, the comparison becomes 0 == false, which is true because 0 and false are considered equal in value comparison." 
-"// The result of the first comparison, which is true, is then compared to 1, i.e., true < 1." 
+"// The expression false && true || true is evaluated considering the precedence of operations in JavaScript, where logical AND (&&) has higher precedence than logical OR (||)." 
+"// First, the operation false && true is performed. Since the first operand is false, this expression will return false." 
+"// Then, the operation false || true is performed. Since the first operand is false and the second operand is true, this expression will return true." 
 "// Therefore, the result of this expression will be true." 
