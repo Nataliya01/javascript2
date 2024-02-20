7a2cc58dea0d524d4fc5230902a62aee9312b4b5
Nataliya Home <vmsoft.ua@gmail.com>
2024-02-20 18:40:37 +0200

    '4px' - 3

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-"// The expression true + false will be evaluated in JavaScript." 
-"// JavaScript automatically converts true to 1 and false to 0 when performing arithmetic operations." 
-"// So, true + false becomes 1 + 0." 
-"// Therefore, the result of this operation will be 1." 
+"// The expression '4px' - 3 attempts to subtract a number from a string, which is not a valid operation in JavaScript, so it will return NaN (Not a Number)." 
