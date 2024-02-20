c8ae1e3c345be43fd5070a4e32b0742a5deba9b4
Nataliya Home <vmsoft.ua@gmail.com>
2024-02-20 19:06:17 +0200

    false && (false || true);

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"// The expression false && true || true is evaluated considering the precedence of operations in JavaScript, where logical AND (&&) has higher precedence than logical OR (||)." 
-"// First, the operation false && true is performed. Since the first operand is false, this expression will return false." 
-"// Then, the operation false || true is performed. Since the first operand is false and the second operand is true, this expression will return true." 
-"// Therefore, the result of this expression will be true." 
+"// The expression false && (false || true) is evaluated considering the precedence of operations in JavaScript." 
+"// First, the operation inside the parentheses (false || true) is performed. The OR operator (||) will return true because one of the operands is true." 
+"// Then, the operation false && true is performed. Since one operand is false and the other is true, this expression will return false." 
+"// Therefore, the result of this expression will be false." 
