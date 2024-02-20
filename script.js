f5c3182e9672f2518424cf80257c8e1f0a7d60ef
Nataliya Home <vmsoft.ua@gmail.com>
2024-02-20 18:50:32 +0200

    '6' + 3 ** 0;

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-"// The expression '4px' - 3 attempts to subtract a number from a string, which is not a valid operation in JavaScript, so it will return NaN (Not a Number)." 
+"// The expression '6' + 3 ** 0 is evaluated in two stages:" 
+"// 3 ** 0 is calculated as 3 to the power of 0, which equals 1." 
+"// Then, string concatenation is performed, where '6' is concatenated with the result of the previous step, which is the number 1." 
+"// Therefore, the result will be the string '61'." 
