0e14adea71adf47b1a5750e61e66bcd935d3c105
Nataliya Home <vmsoft.ua@gmail.com>
2024-02-20 19:01:00 +0200

    null == ''

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-"// The expression '10' + (5 === 6) is evaluated in two stages:" 
-"// The expression (5 === 6) evaluates to false, because 5 is not equal to 6." 
-"// Then, string concatenation is performed, where '10' is concatenated with the result of the previous step." 
-"// Therefore, the result will be the string '10false'." 
+"// The expression null == '' checks if the value null is equal to the empty string ''. In JavaScript, this comparison will return false because null and the empty string are different data types." 
