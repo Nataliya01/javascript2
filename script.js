7b0b15661d7103e1268a171edcbf6a916df9c566
Nataliya Home <vmsoft.ua@gmail.com>
2024-02-20 19:02:39 +0200

    3 ** (9 / 3);

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-"// The expression null == '' checks if the value null is equal to the empty string ''. In JavaScript, this comparison will return false because null and the empty string are different data types." 
+"// The expression 3 ** (9 / 3) calculates the exponentiation of 3 raised to the power computed as the result of dividing 9 by 3." 
+"// The division 9 / 3 evaluates to 3." 
+"// Then exponentiation is performed: 3 ** 3, which equals 27." 
+"// Therefore, the result of this expression will be 27." 
